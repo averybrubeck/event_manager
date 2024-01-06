2686fd3165f8c40a9fbe4d73cf7e737c9cb435fe
averybrubeck <averybrubeck@yahoo.com>
2024-01-06 12:40:34 -0500

    created mini csv parser

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,1 +1,13 @@
 puts 'Event Manager Initialized!'
+
+begin
+  lines = File.readlines('event_attendees.csv')
+  lines.each_with_index do |line, index|
+    next if index.zero?
+    columns = line.split(',')
+    name = columns[2]
+    puts name
+  end
+rescue Errno::ENOENT
+  puts 'File Not Found'
+end
