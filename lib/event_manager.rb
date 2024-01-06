bc2daba9efbbe3b58f72a41faf13a2a21885086b
averybrubeck <averybrubeck@yahoo.com>
2024-01-06 16:54:38 -0500

    refactored code, fixed styling

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -2,8 +2,6 @@ require 'csv'
 require 'erb'
 require 'google/apis/civicinfo_v2'
 
-
-
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
 end
@@ -31,7 +29,7 @@ def save_thankyou_letter(id, form_letter)
   end
 end
 
-def clean_phone_numbers(phonenumber)
+def clean_phone_number(phonenumber)
   if phonenumber.nil? || phonenumber.length < 10
     phonenumber = '0000000000'
   elsif phonenumber.length == 11 && phonenumber[0] == 1
@@ -42,41 +40,37 @@ def clean_phone_numbers(phonenumber)
   phonenumber
 end
 
-def time_targeting(reg_date)
-  begin
-    formatted_date = DateTime.strptime(reg_date,"%m/%d/%y %H:%M")
-  rescue ArgumentError => e
-    puts "Error parsing date: #{e.message}"
-    formatted_date = "0000-00-00 00:00:00"
-  end
-  formatted_date
+def count_frequency(array)
+  array.max_by { |a| array.count(a) }
 end
 
 puts 'EventManager initialized.'
-begin
-  contents = CSV.open(
-    'event_attendees.csv',
-    headers: true,
-    header_converters: :symbol
-  )
-rescue Errno::ENOENT
-  puts 'File not found'
-end
-
-begin
-  template_letter = File.read('form_letter.erb')
-  erb_template = ERB.new template_letter
-rescue Errno::ENOENT
-  puts 'File not found'
-end
-
+contents = CSV.open 'event_attendees.csv', headers: true, header_converters: :symbol
+contents_size = CSV.read('event_attendees.csv').length
+template_letter = File.read 'form_letter.erb'
+erb_template = ERB.new template_letter
+contents_size-=1
+hour_of_day = Array.new(contents_size)
+day_of_week = Array.new(contents_size)
+j = 0
+cal = { 0 => 'sunday', 1 => 'monday', 2 => 'tuesday', 3 => 'wednesday', 4 => 'thursday', 5 => 'friday', 6 => 'saturday' }
 contents.each do |row|
-  id = row [0]
+  id = row[0]
   name = row[:first_name]
-  phone = clean_phone_numbers(row[:homephone])
-  time = time_targeting(row[:regdate])
   zipcode = clean_zipcode(row[:zipcode])
   legislators = legislators_by_zipcode(zipcode)
+  reg_date = row[:regdate]
+  untidy_numbers = row[:homephone]
+  phone_number = clean_phone_number(row[:homephone])
+  i = ((row[0].to_f/contents_size)*100)
+  puts "------------#{i.round(2)}%-----------"
+  reg_date_to_print = DateTime.strptime(reg_date, '%m/%d/%y %H:%M')
+  hour_of_day[j] = reg_date_to_print.hour
+  day_of_week[j] = reg_date_to_print.wday
+  j += 1
+
   form_letter = erb_template.result(binding)
   save_thankyou_letter(id, form_letter)
 end
+puts "Most Active Hour is : #{count_frequency(hour_of_day)}"
+puts "Most Active Day is : #{cal[count_frequency(day_of_week)].capitalize}"
