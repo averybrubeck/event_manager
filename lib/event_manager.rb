8c418c7e658bb58f4bbeda7ca5758390c47ae144
averybrubeck <averybrubeck@yahoo.com>
2024-01-06 15:39:09 -0500

    added erb form and created output generation

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,5 +1,8 @@
 require 'csv'
+require 'erb'
 require 'google/apis/civicinfo_v2'
+
+
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
@@ -9,15 +12,22 @@ def legislators_by_zipcode(zip)
   civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
   civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
   begin
-    legislators = civic_info.representative_info_by_address(
+    civic_info.representative_info_by_address(
       address: zip,
       levels: 'country',
       roles: ['legislatorUpperBody', 'legislatorLowerBody']
-    )
-    legislators = legislators.officials.map(&:name).join(', ')
+    ).officials
   rescue StandardError => e
     "An error occured #{e.message} You can find your representatives by visiting
     www.commoncause.org/take-action/find-elected-officials"
+  end
+end
+
+def save_thankyou_letter(id, form_letter)
+  Dir.mkdir('Output') unless Dir.exist?('Output')
+  filename = "Output/Thanks_#{id}.html"
+  File.open(filename, 'w') do |file|
+    file.puts form_letter
   end
 end
 
@@ -32,9 +42,18 @@ rescue Errno::ENOENT
   puts 'File not found'
 end
 
+begin
+  template_letter = File.read('form_letter.erb')
+  erb_template = ERB.new template_letter
+rescue Errno::ENOENT
+  puts 'File not found'
+end
+
 contents.each do |row|
+  id = row [0]
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
   legislators = legislators_by_zipcode(zipcode)
-  puts "#{name} #{zipcode} #{legislators}"
+  form_letter = erb_template.result(binding)
+  save_thankyou_letter(id, form_letter)
 end
